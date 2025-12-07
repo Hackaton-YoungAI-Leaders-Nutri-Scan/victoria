19fc687d9cf97beb0e3710262046adfb14cdab0e
johnmolano <jhon.molano7@gmail.com>
2025-12-07 05:21:13 -0500

    add icons

---
+++ frontend/app.config.js
@@ -0,0 +1,24 @@
+export default {
+  expo: {
+    name: 'Victoria',
+    slug: 'victoria-app',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.jpg',
+      resizeMode: 'contain',
+      backgroundColor: '#FFFFFF',
+    },
+    android: {
+      package: 'com.victoria.app',
+      adaptiveIcon: {
+        foregroundImage: './assets/icon.png',
+        backgroundColor: '#FFFFFF',
+      },
+      splash: {
+        image: './assets/splash.jpg',
+        resizeMode: 'contain',
+        backgroundColor: '#FFFFFF',
+      },
+    },
+  },
+}
